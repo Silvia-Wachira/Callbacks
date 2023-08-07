f32360daf43cc0c60d76b9354c744ace725cbd36
Silvia-Wachira <slywachira@gmail.com>
2023-08-07 11:37:59 +0300

    create a list

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-const buttonTwo = document.querySelector('.btn-2')
+// const buttonTwo = document.querySelector('.btn-2')
 
-function alertBtn() {
-    alert('I also love JavaScript');
-}
+// function alertBtn() {
+//     alert('I also love JavaScript');
+// }
 
-buttonTwo.addEventListener('click', alertBtn)
+// buttonTwo.addEventListener('click', alertBtn)
 
 //mouseover event
 
-const newBackgroundColor = document.querySelector(".box-3")
+// const newBackgroundColor = document.querySelector(".box-3")
 
-function changeBgColor() {
-    newBackgroundColor.style.backgroundColor = 'red';
-}
+// function changeBgColor() {
+//     newBackgroundColor.style.backgroundColor = 'red';
+// }
 
-newBackgroundColor.addEventListener('mouseover', changeBgColor)
+// newBackgroundColor.addEventListener('mouseover', changeBgColor)
 
-let name = prompt("What's your name?");
-document.getElementById('welcome').innerHTML = "welcome:" + name
+// let name = prompt("What's your name?");
+// document.getElementById('welcome').innerHTML = "welcome:" + name
+
+
+// const todo = ['Homework', 'Food', 'Project', 'Learn JS']
+
+// for(let i = 0; i < todo.length; i++) {
+//     console.log(todo[i]);
+// }
+
+
+
+
+
