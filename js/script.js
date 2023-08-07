6c36e96385ecaddddee597a2275dcf418821a8b2
Silvia-Wachira <slywachira@gmail.com>
2023-08-07 17:23:00 +0300

    select nth child

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ console.log(tagline)
 let image = document.querySelector('.card img')
 console.log(image);
 
+let link = document.querySelector('#primary a:first-child')
+let link = document.querySelector('#primary a:last-child')
+let link = document.querySelector('#primary a:nth-child(2)')
+
+console.log(link)
