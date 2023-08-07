29794cb395733e3565d16efbb26549f75fe2b9da
Silvia-Wachira <slywachira@gmail.com>
2023-08-07 17:19:38 +0300

    select element from the dom:

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ const tagline = document.querySelector('.tagline');
 const newText = tagline.textContent.replace('$15', '$12');
 tagline.textContent = newText
 console.log(tagline)
+
+let image = document.querySelector('.card img')
+console.log(image);
+
