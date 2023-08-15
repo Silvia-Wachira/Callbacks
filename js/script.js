902efa95ab3ab33919a14eff3f5f26ff305f413e
Silvia-Wachira <slywachira96@gmail.com>
2023-08-15 13:07:30 +0300

    create a target

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
-const clearCartBtn = document.getElementById('clear-cart');
-clearCartBtn.addEventListener('click', clearbtnFunction);
+//Mouse Events
+//Create the variables
 
-function clearbtnFunction(e) {
-    //target
-    let element;
-
-    element = e;
-
-    element = e.target;
-    element = e.target.id
-    element = e.target.innerText
-    element = e.target.innerText = 2 + 2;
-
-
-
-    console.log(element)
-}
+const heading = document.querySelector
