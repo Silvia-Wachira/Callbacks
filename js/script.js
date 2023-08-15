a457b536489dbcd5b6ebd72859c8d06cd8b3271a
Silvia-Wachira <slywachira96@gmail.com>
2023-08-15 14:22:16 +0300

    submit event

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//Input 
+//Input  & Form events
+
+//Create the variables
+
+const searchForm = document.getElementById('search'),
+      searchInput = document.getElementById('search-course')
+
+searchForm.addEventListener('submit', printEvent)
+
+function printEvent(e) {
+    e.preventDefault()
+
+    //Print the value from the input
+    console.log(searchInput.value)
+    // console.log(`Type: ${e.type}`)
+}
