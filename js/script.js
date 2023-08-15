0e0493d0060537a711160c396ce52ed3faeabd54
Silvia-Wachira <slywachira96@gmail.com>
2023-08-15 13:39:27 +0300

    create a mouse event

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@
 //Create the variables
 
 const heading = document.querySelector('#heading')
-const limks = document.querySelector('nav')
+const links = document.querySelector('nav')
 const clearCartBtn = document.querySelector('#clear-cart')
+
+console.log(heading)
+console.log(links)
+console.log(clearCartBtn)
+
+// clearCartBtn.addEventListener('click', printEvent)
+// clearCartBtn.addEventListener('dblclick', printEvent)
+clearCartBtn.addEventListener('mouseenter', printEvent)
+
+function printEvent(e) {
+    // console.log(e)
+    console.log(`THE EVENT IS ${e.type}`)
+}
