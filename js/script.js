92a351c9f2610418b273175c092b9495101084d3
Silvia-Wachira <slywachira96@gmail.com>
2023-08-15 12:58:37 +0300

    0

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const clearCartBtn = document.getElementById('clear-cart');
 clearCartBtn.addEventListener('click', clearbtnFunction);
 
-function clearbtnFunction() {
-    console.log('You clicked a btn')
+function clearbtnFunction(e) {
+    //target
+    let element;
+
+    element = e;
+
+    element = e.target;
+    element = e.target.id
+    element = e.target.innerText
+    element = e.target.innerText = 2 + 2;
+
+
+
+    console.log(element)
 }
