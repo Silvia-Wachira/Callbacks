ab7748ce7b1d5007376c559e2434b91a9456687e
Silvia-Wachira <slywachira96@gmail.com>
2023-08-15 14:40:40 +0300

    create more events

--- js/script.js
+++ js/script.js
@@ -5,12 +5,27 @@
 const searchForm = document.getElementById('search'),
       searchInput = document.getElementById('search-course')
 
-searchForm.addEventListener('submit', printEvent)
+// searchForm.addEventListener('submit', printEvent)
+
+// searchInput.addEventListener('keydown', printEvent )
+
+// searchInput.addEventListener('keyup', printEvent )
+// searchInput.addEventListener('keypress', printEvent )
+// searchInput.addEventListener('blur', printEvent )
+// searchInput.addEventListener('cut', printEvent )
+// searchInput.addEventListener('copy', printEvent )
+// searchInput.addEventListener('paste', printEvent )
+searchInput.addEventListener('input', printEvent )
+
 
 function printEvent(e) {
-    e.preventDefault()
+    // e.preventDefault()
+
+    if(searchInput.value.length == 0) {
+        alert('Type something please')
+    }
 
     //Print the value from the input
     console.log(searchInput.value)
-    // console.log(`Type: ${e.type}`)
+    console.log(`Type: ${e.type}`)
 }
