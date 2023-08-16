824366755c727aa2482a821183c015745b3c1442
Silvia-Wachira <slywachira96@gmail.com>
2023-08-16 15:16:30 +0300

    add delegations

--- js/script.js
+++ js/script.js
@@ -3,5 +3,20 @@
 document.body.addEventListener('click', removeProductFromCart)
 
 function removeProductFromCart(e) {
-    console.log(e)
+    console.log(e.target.classList)
+
+    if(e.target.classList.contains('remove')){
+        
+        e.target.parentElement.parentElement
+    } 
+}
+
+const courseList = document.querySelector('#courses-list')
+
+courseList.addEventListener('click', addToCart )
+
+function addToCart(e) {
+    if(e.target.classList.contains(add-to-cart)){
+        console.log('Course added!!');
+    }
 }
