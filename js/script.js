1d20f78e35440a62a31b26c7d1b43bd78f28f6ad
Silvia-Wachira <slywachira96@gmail.com>
2023-08-16 13:52:11 +0300

    add a stip propagation function

--- js/script.js
+++ js/script.js
@@ -9,13 +9,18 @@ const card = document.querySelector('.card'),
  //Event Listeners
 card.addEventListener('click', function(e) {
     console.log('You clicked the cart')
+    e.stopPropagation();
 })
 
 infoCards.addEventListener('click', function(e) {
     console.log('You clicked the Info cart')
+    e.stopPropagation();
+
 })
 
 addCartBtn.addEventListener('click', function(e) {
 
     console.log('You clicked the add-cart btn')
+    e.stopPropagation();
+
 })
