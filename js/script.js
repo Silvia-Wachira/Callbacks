45982b08f4e4dbc22c1eaf260e942470c37ecc26
Silvia-Wachira <slywachira96@gmail.com>
2023-08-16 14:57:33 +0300

    create a function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,7 @@
-//Event Bubbling
-//Variables
-const card = document.querySelector('.card'),
-      infoCards = document.querySelector('.info-cart'),
-      addCartBtn = document.querySelector('.add-to-cart')
-      
+//Delegation
 
+document.body.addEventListener('click', removeProductFromCart)
 
- //Event Listeners
-card.addEventListener('click', function(e) {
-    console.log('You clicked the cart')
-    e.stopPropagation();
-})
-
-infoCards.addEventListener('click', function(e) {
-    console.log('You clicked the Info cart')
-    e.stopPropagation();
-
-})
-
-addCartBtn.addEventListener('click', function(e) {
-
-    console.log('You clicked the add-cart btn')
-    e.stopPropagation();
-
-})
+function removeProductFromCart(e) {
+    console.log(e)
+}
