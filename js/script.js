5f1b3a5d98ce8ae0ebcbf5bdf9d8b8dcc2802dad
Silvia-Wachira <slywachira96@gmail.com>
2023-08-17 15:45:08 +0300

    get item in local storage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
 const localStorageContent = localStorage.getItem('name')
 
-
-let names;
-if(localStorageContent === null){
-    names = [];
-} 
-
-console.log(names)
-
-const myArray = [1, 2, 3];
-
-console.log(JSON.stringify(myArray))
-
-console.log(localStorageContent);
+console.log(localStorageContent)
