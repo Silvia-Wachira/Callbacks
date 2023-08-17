88d27fdd3d922f7c5f29ecd6a25978d70af4b03b
Silvia-Wachira <slywachira96@gmail.com>
2023-08-17 10:14:51 +0300

    add to local storage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
-//Delegation
+//Local Storage
 
-document.body.addEventListener('click', removeProductFromCart)
+//Add to local storage
+localStorage.setItem('name', 'Juan');
 
-function removeProductFromCart(e) {
-    console.log(e.target.classList)
-
-    if(e.target.classList.contains('remove')){
-        
-        e.target.parentElement.parentElement
-    } 
-}
-
-const courseList = document.querySelector('#courses-list')
-
-courseList.addEventListener('click', addToCart )
-
-function addToCart(e) {
-    if(e.target.classList.contains(add-to-cart)){
-        console.log('Course added!!');
-    }
-}
+//Add to session storage
+sessionStorage.setItem('name', 'Juan')
