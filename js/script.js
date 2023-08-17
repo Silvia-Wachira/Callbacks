cd72c37a0b6b93d71738fce915be0e47ac0aea7f
Silvia-Wachira <slywachira96@gmail.com>
2023-08-17 10:28:01 +0300

    clear local storage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 
 //Add to local storage
-localStorage.setItem('name', 'Juan');
+// localStorage.setItem('name', 'Juan');
 
 
 //remove from local storage
-localStorage.removeItem('name')
+// localStorage.removeItem('name')
+
+//rreead th e value
+
+// const name = localStorage.getItem('name')
+
+//clear storage
+// localStorage.clear();
+
+localStorage.setItem('name1', 'Juan');
+localStorage.setItem('name2', 'Walter');
