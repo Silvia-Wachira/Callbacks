df54a47e1d60bb6fb44fd60743891cfbce54d4a6
Silvia-Wachira <slywachira96@gmail.com>
2023-09-28 20:41:08 +0300

    checck for balance

--- js/script.js
+++ js/script.js
@@ -3,8 +3,21 @@
 function Client(namw, balance) {
     this.name = name;
     this.balance = balance
+    this.membership = function() {
+        let name;
+        //check for the balance
+        if(this.balance > 1000) {
+            name = 'Gold';
+        } else if (this.balance > 500) {
+            name = 'Platinum'
+        } else {
+            name = 'Normal'
+        }
+    }
 }
 
 const person = new Client('Juan', 1000); 
+const person2 = new Client('Karen', 600);
 
 console.log(person);
+console.log(person2);
