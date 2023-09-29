ee93bc9e008a759ad58396635a2dc04052554543
Silvia-Wachira <slywachira96@gmail.com>
2023-09-29 14:08:34 +0300

    log a function

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function Client(name, balance) {
 Client.prototype.membership = function() {
     let name;
     
-
         //check for the balance
         if(this.balance > 1000) {
             name = 'Gold';
@@ -27,3 +26,8 @@ const person = new Client('Juan', 100);
 const person2 = new Client('Karen', 600);
 
 console.log(Client.prototype)
+
+console.log(person)
+console.log(person.membership())
+console.log(person2)
+console.log(person2.membership())
