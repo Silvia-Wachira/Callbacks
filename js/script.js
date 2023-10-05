9a7c2e4d305151ef74f533d0fea3af8781181d98
Silvia-Wachira <slywachira96@gmail.com>
2023-10-05 20:31:15 +0300

    0

--- js/script.js
+++ js/script.js
@@ -27,19 +27,46 @@ Client.prototype.clientInfo = function() {
 }
 
 //Another method to withdraw money from the account
-Client.prototype.withdraw = function() {
-    return `Name: ${this.name}, Balance ${this.balance}, Membership: ${this.membership() }`
+Client.prototype.withdraw = function(amount) {
+     this.balance -= amount;
 }
 
+//create a deposit method
+Client.prototype.deposit = function(amount) {
+    this.balance += amount;
+}
 
-const person = new Client('Juan', 100); 
+//check the balance
+Client.prototype.getBalance = function() {
+    return this.balance
+}
+
+const person = new Client('Juan', 2000); 
 const person2 = new Client('Karen', 600);
 
 console.log(person)
 console.log(person.membership() );
 console.log(person.clientInfo() );
+person.withdraw(100)
+console.log(person.clientInfo() );
+console.log(person.getBalance() );
+person.deposit(500);
+console.log(person.clientInfo() );
+console.log(person.getBalance() );
+
+
+console.log('------------');
 
 console.log(person2);
 console.log(person2.membership());
 console.log(person2.clientInfo());
 
+console.log(person2)
+console.log(person2.membership() );
+console.log(person2.clientInfo() );
+person2.withdraw(1000)
+console.log(person2.clientInfo() );
+console.log(person2.getBalance() );
+person2.deposit(500);
+console.log(person2.clientInfo() );
+console.log(person2.getBalance() );
